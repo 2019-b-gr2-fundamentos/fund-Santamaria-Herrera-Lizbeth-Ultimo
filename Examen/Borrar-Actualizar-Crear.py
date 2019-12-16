6f39bebccd213f42341cc9e416365a689b78fa83
Lizbeth Santamaria <lizbeth.santamaria@epn.edu.ec>
2019-12-16 14:08:30 -0500

    Examen

--- Examen/Borrar-Actualizar-Crear.py
+++ Examen/Borrar-Actualizar-Crear.py
@@ -8,6 +8,7 @@ def main():
 
 def dinosaurios():
     print (" ",DINOSAURIOS)
+    print("OBSERVACION: EL INDICEDEL ARREGLO INICIA EN 0")
     print ("Selecciona una opcion:\n1.Observar el arreglo de dinosaurios \n2.Añadir o cambiar \n3:Eliminar\n4:Observar un dinosaurio en especifico\n5:Salir")
     print("Ingresa el numero de la opcion que deseas que se ejecute")
     opcion = int(input())
@@ -25,18 +26,65 @@ def dinosaurios():
             print("opcion no valida")
 
     elif opcion == 2:
-        print("Ingrese el nombre del dinosaurio que desea añadir")
-        agregar = str(input())
-        DINOSAURIOS.append(agregar)
-        print("Ël arreglo con un dinisaurio agregado es: ", DINOSAURIOS)
-        print("1:menú principal\n2:salir")
-        op = int(input())
-        if op == 1:
-            main()
-        elif op == 2:
-            print("Adios")
+        print("Elija:\n1:Añadir\n2:actualizar")
+        eli = int(input())
+        if eli ==1:
+            print("Ingrese el nombre del dinosaurio que desea añadir")
+            agregar = str(input())
+            DINOSAURIOS.append(agregar)
+            print("Ël arreglo con un dinisaurio agregado es: ", DINOSAURIOS)
+            print("1:menú principal\n2:salir")
+            op = int(input())
+            if op == 1:
+                main()
+            elif op == 2:
+                print("Adios")
+            else:
+                print("opcion no valida")
+
+        elif eli == 2:
+            print("1:Ingresar nombre\n2:random")
+            nom = int(input())
+            
+            if nom == 1:
+                print("Ingrese la posicion que desea actualizar")
+                posi = int(input())
+                if  0 <= posi <= len(DINOSAURIOS)-1 :
+                    DINOSAURIOS[posi] = str(input("Ingrese nombre de dinosaurio: "))
+                    print("El arreglo actualizado es:", DINOSAURIOS)
+                    print("1:menú principal\n2:salir")
+                    op = int(input())
+                    if op == 1:
+                     main()
+                    elif op == 2:
+                        print("Adios")
+                    else:
+                        print("opcion no valida")
+                else:
+                    print("Posicion invalida")
+
+            if nom == 2:
+                print("Ingrese la posicion que desea actualizar")
+                posi = int(input())
+                if  0 <= posi <= len(DINOSAURIOS)-1 :
+                    DINOSAURIOS[posi] = random.choice(DINOSAURIOS)
+                    print("El arreglo actualizado es:", DINOSAURIOS)
+                    print("1:menú principal\n2:salir")
+                    op = int(input())
+                    if op == 1:
+                     main()
+                    elif op == 2:
+                        print("Adios")
+                    else:
+                        print("opcion no valida")
+                else:
+                    print("Posicion no valida")
+
+            
         else:
-            print("opcion no valida")
+            print("Posicion invalida")
+
+        
 
     elif opcion == 3:
         print ("El arreglo es : ", DINOSAURIOS)
